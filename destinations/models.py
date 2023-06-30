f75155b1de2a03a4382352b6ef962aa9dda19506
James Gulland <jamesgulland@gmail.com>
2023-06-30 14:55:26 +0100

    small modifications to Destination model

--- destinations/models.py
+++ destinations/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 class Destination(models.Model):
     name = models.CharField(max_length=100)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
     country = models.CharField(max_length=100)
     continent = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
     description = models.TextField(max_length=300)
+
+    def __str__(self):
+        return f"{self.name}, {self.country}"
