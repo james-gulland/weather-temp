99a5439fd9c0373766950bcd97c32ad606ddb3b4
James Gulland <jamesgulland@gmail.com>
2023-07-04 11:22:14 +0100

    adding serializer for common Destination data

---
+++ destinations/serializers/common.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+from ..models import Destination
+
+# Create custom Serializer extending ModelSerializer that will convert our querysets into python data types and vice versa
+class DestinationSerializer(ModelSerializer):
+    # This will include a Meta subclass that specifies the model we're querying and the fields we want to serialize/deserialize
+    class Meta:
+        model = Destination # this is the model to query when making requests
+        fields = '__all__' # __all__ allows us to include all fields rather than specifying them individually. You can use a list or tuple of names of fields as strings if you want to be specific
--- destinations/views.py
+++ destinations/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+# from rest_framework import status
+from .serializers.common import DestinationSerializer
+from .models import Destination
 
 # Create your views here.
 class DestinationViewList(APIView):
     
     def get(self, request):
       print('GET /api/destinations/ endpoint hit')
-      return Response('GET /api/destinations/ endpoint hit')
+
+      destinations = Destination.objects.all()
+      serialized_destinations = DestinationSerializer(destinations, many=True)
+
+      return Response(serialized_destinations.data)
+      # return Response('GET /api/destinations/ endpoint hit')
