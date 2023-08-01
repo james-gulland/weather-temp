c16cb06dd6c248453e773b80c5603f92304617af
James Gulland <jamesgulland@gmail.com>
2023-08-01 14:52:34 +0100

    adding image parameters in database and migrating

--- destinations/admin.py
+++ destinations/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Destination
+from .models import Destination, Image
+
+# Define the ImageInline class for inline display
+class ImageInline(admin.StackedInline):  # Use TabularInline if you prefer a tabular layout.
+    model = Image
+
+# Customize the DestinationAdmin class to include the ImageInline
+class DestinationAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
 
 # Register your models here.
-admin.site.register(Destination)
+admin.site.register(Destination, DestinationAdmin)  # Use DestinationAdmin for customizing the Destination admin view
+admin.site.register(Image)  # Register the Image model separately to access it directly in the admin
--- destinations/models.py
+++ destinations/models.py
@@ -12,3 +12,13 @@ class Destination(models.Model):
 
     def __str__(self):
         return f"{self.id}: {self.name}, {self.country}"
+    
+# model for Images here (tied closely to Desintations)
+class Image(models.Model):
+    id = models.AutoField(primary_key=True)
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name='images')
+    is_primary = models.BooleanField(default=False)
+    image_parameter = models.CharField(max_length=100, unique=True, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.destination.name} Image ({self.id})"
