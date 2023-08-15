eefd20b24d326b792036055565c47ffbd1fb460c
James Gulland <jamesgulland@gmail.com>
2023-08-15 16:25:27 +0100

    adding axios request for destination based on slug

--- client/src/components/DestinationSingle.tsx
+++ client/src/components/DestinationSingle.tsx
@@ -1,7 +1,29 @@
 import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Destination } from '../types/interfaces'
+import axios from 'axios'
 
 const DestinationSingle = () => {
+
+  // grab the slug data from the params, to cross-reference with API call
   const { slug } = useParams()
+  const [destination, setDestination] = useState<Destination[]>([])
+
+  useEffect(() => {
+
+    // on pageload, get destination data based on the entered slug
+    const getData = async (): Promise<void> => {
+      try { 
+        const { data } = await axios.get<Destination[]>(`/api/destinations/${slug}/`)
+        setDestination(data)
+        console.log('Slug data', data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getData()
+  }, [])
+
   return (
     <div>Hello {slug}!</div>
   )
