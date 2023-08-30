099dbee2d321b1c24b3c150395b5a0e0f7bcdc8d
James Gulland <jamesgulland@gmail.com>
2023-08-30 12:07:53 +0100

    grabbing data for climate component successfully

--- client/src/components/ClimateOverview.tsx
+++ client/src/components/ClimateOverview.tsx
@@ -7,9 +7,15 @@ interface Props {
 const ClimateOverview: React.FC<Props> = ({ weatherdata }) => {
   return (
     <>
-    {weatherdata.map((data, i) => {
+    {weatherdata.map((data) => {
         return (
-          <div key={data.id}>{data.month}</div>
+          <div key={data.id}>
+            <div>{data.month}</div>
+            <div>Avg feels like: {data.average_feels_like_temperature}</div>
+            <div>Avg temp: {data.average_temperature}</div>
+            <div>Humidity: {data.relative_humidity}</div>
+            <div>Avg rainfall: {data.precipitation_levels}</div>
+          </div>
         )
       })
     }
