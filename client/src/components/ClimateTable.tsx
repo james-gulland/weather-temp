bd5d6b3bf8cb27057dc1a9daf9456d98a296a831
James Gulland <jamesgulland@gmail.com>
2023-09-18 15:34:54 +0100

    adding temp units to table

--- client/src/components/ClimateTable.tsx
+++ client/src/components/ClimateTable.tsx
@@ -20,25 +20,25 @@ const ClimateTable: React.FC<Props> = ({ weatherdata, tempUnit }) => {
         {/* AVG FEELS ROW */}
         <tr>
           <td>{weatherOptions[1].label}</td>
-          {weatherdata.map((month) => <td key={month.id} className="table-climate-data">{tempUnit === 'C' ? month.average_feels_like_temperature : celsiusToFahrenheit(month.average_feels_like_temperature)}</td>)}
+          {weatherdata.map((month) => <td key={month.id} className="table-climate-data">{tempUnit === 'C' ? month.average_feels_like_temperature : celsiusToFahrenheit(month.average_feels_like_temperature)}°{tempUnit}</td>)}
         </tr>
 
         {/* AVG TEMP ROW */}
         <tr>
           <td>{weatherOptions[0].label}</td>
-          {weatherdata.map((month) => <td key={month.id} className="table-climate-data">{tempUnit === 'C' ? month.average_temperature : celsiusToFahrenheit(month.average_temperature)}</td>)}
+          {weatherdata.map((month) => <td key={month.id} className="table-climate-data">{tempUnit === 'C' ? month.average_temperature : celsiusToFahrenheit(month.average_temperature)}°{tempUnit}</td>)}
         </tr>
 
         {/* REL HUMIDITY ROW */}
         <tr>
           <td>{weatherOptions[2].label}</td>
-          {weatherdata.map((month) => <td key={month.id} className="table-climate-data">{month.relative_humidity}</td>)}
+          {weatherdata.map((month) => <td key={month.id} className="table-climate-data">{month.relative_humidity}%</td>)}
         </tr>
 
         {/* PRECIPITATION ROW */}
         <tr>
           <td>{weatherOptions[3].label}</td>
-          {weatherdata.map((month) => <td key={month.id} className="table-climate-data">{month.precipitation_levels}</td>)}
+          {weatherdata.map((month) => <td key={month.id} className="table-climate-data">{month.precipitation_levels}mm</td>)}
         </tr>
       </tbody>
     </table>
